9eaefd76e3b3b7e30eb7e912215604d10690a4b7
akimasa-l <liumingzheng8@outlook.com>
2021-06-21 19:37:53 +0900

    Pythonでやる必要なかったわ

--- authentication.py
+++ authentication.py
@@ -1,1 +1,18 @@
-url="https://foursquare.com/oauth2/authenticate?client_id=YOUR_CLIENT_ID&response_type=code&redirect_uri=YOUR_REGISTERED_REDIRECT_URI"
+import requests
+
+with open("./client_id.txt") as f:
+    client_id = f.read().rstrip()
+with open("./client_secret.txt") as f:
+    client_secret = f.read().rstrip()
+
+print("client_id, client_secret = ", client_id, client_secret)
+
+redirect_uri="https://twitter.com/Akimasa_L"
+
+url = f"https://foursquare.com/oauth2/authenticate?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}"
+
+print(url)
+
+response=requests.get(url)
+
+print(response.url)
