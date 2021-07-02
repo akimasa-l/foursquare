68e1be9821e27354d136de431a3a402d8d7bb8a4
akimasa-l <liumingzheng8@outlook.com>
2021-07-02 17:02:50 +0900

    Create search_place.py

---
+++ search_place.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import datetime
+
+longitude_latitude="35.42252723830785,139.64312939950295"
+
+url="https://api.foursquare.com/v2/venues/search"
+
+with open("./access_token.txt") as f:
+    access_token = f.read().rstrip()
+
+params={
+    "oauth_token":access_token,
+    "ll":longitude_latitude,
+    "v":datetime.date.today().strftime("%Y%m%d")
+}
+
+response =requests.get(url,params=params)
+
+print(response.url)
+
+a=json.loads(response.text)
+
+print(json.dumps(a, indent=4))
